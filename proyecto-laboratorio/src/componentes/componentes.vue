9ffc8203eb2e35fd35e24af6d248ccd6aa8ce2fb
Thorling006 <venturaomar569@gmail.com>
2026-03-16 19:54:44 -0600

    Cambios

---
+++ proyecto-laboratorio/src/componentes/componentes.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+const nombre = ref('');
+const apellido = ref('');
+const correo = ref('');
+const contraseña = ref('');
+
+const registrarse = () => {
+    console.log(nombre.value, apellido.value, correo.value, contraseña.value);
+} 
+</script>
+
+<template>
+    <input type="text" placeholder="Nombre">
+    <input type="text" placeholder="Apellido">
+    <input type="text" placeholder="Correo">
+    <input type="text" placeholder="Contraseña">
+    <button>Registrarse</button>
+</template>
+
+<style scoped>
+    input {
+        margin: 10px;
+        padding: 10px;
+    }
+    button {
+        margin: 10px;
+        padding: 10px;
+    }
+</style>
